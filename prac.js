913df00d0329b8142aea0be503d48244ff26366c
vedamshpawar <vedamshpawar@gmail.com>
2025-12-27 16:37:51 +0530

    practiced js

---
+++ prac.js
@@ -0,0 +1,99 @@
+// function my_function() {
+//     document.getElementById('demo').innerHTML='hoorahh! how are you...';
+// }
+
+// var x = 201;
+// var x = 300;
+// console.log(x);
+
+// let a = 1000;
+// let a = 20000;
+// console.log(b);
+
+// const ban = 20;
+// console.log(ban);
+
+// let a = "hello";
+// let b = 4;
+// console.log(a + b)
+
+// let set = "hello";
+// let tes = "hello";
+// console.log(set == tes);
+
+// let x = 10;
+// x++;
+// let z = x;
+// console.log(z);
+
+
+// let a = 21;
+// a--;
+// let b = a;
+// console.log(typeof(b));
+
+// let s = "5"; 
+// let r = 5;
+// console.log(typeof(s == r));
+
+
+// const age = prompt('enter a age: ');
+// if (age < 18){
+//     console.log("wait until you turn age 18");
+// }else if (age >= 18){
+//     console.log("eligible for vote");
+// }else{
+//     console.log('your a child be patience');
+// };
+
+
+// const age = prompt('enter your age: ');
+// console.log(age >= 18 ? 'can vote': 'you are not eligible');
+
+// switch (new Date().getDay()){
+//     case 0:
+//         day = 'sunday';
+//         break;
+//     case 1:
+//         day = 'Monday';
+//         break;
+//     case 2:
+//         day = 'Tuesday';
+//         break;
+//     case 3:
+//         day = 'wednesday';
+//         break;
+//     case 4:
+//         day = 'Thursday';
+//         break;
+//     case 5:
+//         day = 'Friday';
+//         break;
+//     case 6:
+//         day = 'saturday';
+//         break;
+
+// }
+
+// let i = 10;
+// for(i = 0; i < 11; i++){
+//     console.log(i)
+// }
+
+// let i = 5;
+// for(i = 0; i < 5; i--){
+//     console.log(i)
+// }
+
+// let i = 10;
+// for(let i = 0; i <= 10; i++){
+//     console.log(i)
+// }
+
+// let i = 0;
+// while(i <= 10){
+//     text = "the number is "+i+" here";
+//     console.log(text);
+//     i++;
+// }
+
